d1e35d2b6cecd0f19c7702710acdd85a176bc4da
Soubi2 <98765333+Soubi2@users.noreply.github.com>
2022-07-27 00:03:34 +0300

    Update and rename ssb.js

--- ssb.js
+++ ssb.js
@@ -1,5 +1,3 @@
-﻿
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
@@ -12,16 +10,16 @@ function fuckfuck(p, a, c, k, e, d) {
     }return p;
 }
 
-var Sbplayone = function () {
-    function Sbplayone(props) {
-        _classCallCheck(this, Sbplayone);
+var Sbplay = function () {
+    function Sbplay(props) {
+        _classCallCheck(this, Sbplay);
 
         this.libs = props.libs;
         this.settings = props.settings;
         this.state = {};
     }
 
-    _createClass(Sbplayone, [{
+    _createClass(Sbplay, [{
         key: 'checkLive',
         value: function () {
             var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(url) {
@@ -124,7 +122,7 @@ var Sbplayone = function () {
                                 return _context2.abrupt('return', {
                                     host: {
                                         url: url,
-                                        name: "Sbplayone"
+                                        name: "Sbplay"
                                     },
                                     result: results
                                 });
@@ -145,9 +143,9 @@ var Sbplayone = function () {
         }()
     }]);
 
-    return Sbplayone;
+    return Sbplay;
 }();
 
 thisSource.function = function (libs, settings) {
-    return new Sbplayone({ libs: libs, settings: settings });
+    return new Sbplay({ libs: libs, settings: settings });
 };
